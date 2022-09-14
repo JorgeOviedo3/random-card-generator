1c15329114dff1216fd4da369516e374e14f35a8
Jorge Oviedo <jorgeoviedo1999@gmail.com>
2022-09-14 00:41:26 +0000

    my first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
+  let suits = ["♦", "♥", "♠", "♣"];
+
+  const card = document.querySelector(".card");
+  const cardValue = document.querySelector(".value");
+  let randomForValues = Math.floor(Math.random() * 12);
+  cardValue.innerHTML = values[randomForValues];
+
+  const suit = document.querySelector(".suit");
+  const suitRight = document.querySelector(".suit-right");
+  let randomForSuits = Math.floor(Math.random() * 4);
+  suit.innerHTML = suits[randomForSuits];
+  suitRight.innerHTML = suits[randomForSuits];
+  if (suits[randomForSuits] === "♦") {
+    card.classList.add("diamond");
+  }
+  if (suits[randomForSuits] === "♥") {
+    card.classList.add("heart");
+  }
 };
